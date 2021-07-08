1786fd7405f8bcbeca723c29b36a65f49f0989c2
zvhir <zahir@simonix.my>
2021-07-08 16:32:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/minimal/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.minimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtProgress;
+    private ProgressBar progressBar;
+    private int pStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtProgress = (TextView) findViewById(R.id.txtProgress);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (pStatus <= 73) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(pStatus);
+                            txtProgress.setText(pStatus + "kw");
+                        }
+                    });
+                    try {
+                        Thread.sleep(25);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    pStatus++;
+                }
+            }
+        }).start();
+
+
+    }
+}
